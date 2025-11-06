c2a6897aecbeb27a6571a66b61971f2e60eccac2
Juan Pablo Montiel Cervantes <105291715+joltdot@users.noreply.github.com>
2025-11-05 23:57:02 -0600

    Add page

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+      <main className="flex flex-row-reverse min-h-screen items-center justify-center md:flex-row md:gap-5">
+          <h1 className="font-primary rotate-90 text-6xl p-0 sm:rotate-360">
+              funky.engineer
+          </h1>
+          <h2 className="font-secondary rotate-90 text-sm md:text-3xl m-0 p-0 ">
+              soon
+          </h2>
+
+      </main>
+  );
+}
